1a7a014649bee392fdf5ffa43b6af3bf0df73fff
pdebasis7 <pdebasis2013@gmail.com>
2024-03-06 22:23:30 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Debasis p",
+    author_email="pdebasis2013@gmail.com",
+    packages=find_packages()
+)
